2d24ecb1d2116f7f9575f6d4539b7aaee5b387d2
rishabdesai7 <rishabdesai7@gmail.com>
2020-01-26 12:42:23 +0530

    fourth

--- CCN/screens/ForgotPassword.js
+++ CCN/screens/ForgotPassword.js
@@ -1,11 +1,97 @@
 import React from 'react';
-import { StyleSheet, View,ImageBackground} from 'react-native';
+import { StyleSheet, View,ImageBackground,Alert,Platform,ToastAndroid} from 'react-native';
 import { Button, Input, Text} from '@ui-kitten/components';
+import {forgotPwdUrl,resetPwdUrl} from '../urls/urlgenerator';
+
 
 
 export default class ForgotPasswordScreen extends React.Component{
 
+  state={
+    waiting : false,
+    username:'',
+    password:'',
+    otp:'',
+  }
+  otpres =''
+
+  displayMessage = (msg)=>{
+    Platform.OS === 'ios' ? Alert.alert(msg) : ToastAndroid.show(msg, ToastAndroid.SHORT);
+  }
+  confirmPress = async()=>{
+    if(this.state.otp == this.otpres)
+    {
+      var resp = await fetch(resetPwdUrl(this.state.username,this.state.password));
+      switch(resp.status){
+        case 200:
+            this.displayMessage('Password changed!');
+            break;
+        case 400:
+        case 204:
+            this.displayMessage('Invalid Username or Password');
+            break;
+        default:
+             this.displayMessage('OOPSY!Please try Again');
+             break;
+      }
+      this.props.navigation.goBack();
+    }
+    else{
+      Alert.alert('OOPSY!Wrong OTP,try again!');
+      this.setState({waiting:false});
+    }
+  }
+  submitPress = async()=>{
+    var resp = await fetch(forgotPwdUrl(this.state.username));
+    switch(resp.status){
+      case 200:
+          resp = await resp.json();
+          this.otpres = resp['otp'];
+          this.setState({waiting:true});
+          break;
+      case 400:
+      case 204:
+          Alert.alert('Invalid Username');
+          break;
+      default:
+           Alert.alert('OOPSY!Please try Again');
+           break;
+    }
+  }
   render(){
+    if(this.state.waiting)
+    return (
+      <ImageBackground source={require('../assets/images/back.png')} style={styles.backgroundImage} >
+            <View style={styles.headerContainer}>
+              <Text
+                category='h1'
+                status = 'control'
+                >
+                Forgot Password
+              </Text>
+            </View>
+            <View style={styles.formContainer}>
+              <Input
+                placeholder='OTP'
+                value = {this.state.otp}
+                onChangeText={(text) => this.setState({otp:text})}
+              />
+              <Input
+                placeholder='New Password'
+                value = {this.state.password}
+                onChangeText={(text) => this.setState({password:text})}
+              />
+            </View>
+            <Button
+              size='giant'
+              status = 'danger'
+              style={styles.ForgotButton}
+              onPress = {this.confirmPress}
+              >
+              Confirm
+            </Button>
+        </ImageBackground>
+      );
     return (
       <ImageBackground source={require('../assets/images/back.png')} style={styles.backgroundImage} >
             <View style={styles.headerContainer}>
@@ -26,12 +112,15 @@ export default class ForgotPasswordScreen extends React.Component{
             <View style={styles.formContainer}>
               <Input
                 placeholder='UserId'
+                value = {this.state.username}
+                onChangeText={(text) => this.setState({username:text})}
               />
             </View>
             <Button
               style={styles.ForgotButton}
               size='giant'
               status = 'danger'
+              onPress = {this.submitPress}
               >
               Submit
             </Button>
--- CCN/urls/urlgenerator.js
+++ CCN/urls/urlgenerator.js
@@ -1,2 +1,4 @@
 import con from '../json/config';
 export var loginUrl = _=> {return 'http://'+con['ip']+':'+con['port']+'/colcon/login'}
+export var forgotPwdUrl = (id) => {return 'http://'+con['ip']+':'+con['port']+'/colcon/forgotpassword/'+id}
+export var resetPwdUrl = (id,pwd)=>{return 'http://'+con['ip']+':'+con['port']+'/colcon/resetpassword/'+id+'/'+pwd}
