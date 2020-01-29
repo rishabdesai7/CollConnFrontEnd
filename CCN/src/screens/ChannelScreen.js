ca43fa49b4b3870dc639b7f1e34b994d084228aa
rishabdesai7 <rishabdesai7@gmail.com>
2020-01-29 14:34:44 +0530

    seventh

--- CCN/src/screens/ChannelScreen.js
+++ CCN/src/screens/ChannelScreen.js
@@ -1,13 +1,61 @@
 import React from 'react';
-import { Layout,TopNavigation,List,Button,ButtonGroup} from '@ui-kitten/components';
-import {HeartIconFill} from '../assets/icons/index'
+import { Layout,TopNavigation,List,Button,OverflowMenu,TopNavigationAction} from '@ui-kitten/components';
+import {HeartIconFill,MoreVerticalIconFill,PlusIconFill,} from '../assets/icons/index'
 import Post from '../Components/Post.component';
 export default class ChannelScreen extends React.Component{
 
+    state ={
+      menuVisible :false,
+      channel:'College',
+    }
     data = new Array(10).fill({
         title: 'Title for Item',
         description: 'Description for Item',
       });
+    menuData = [
+        {
+          title: 'College',
+        },
+        {
+          title: 'Canteen',
+        },
+        {
+          title: 'Library',
+        },
+        {
+          title: 'Placement',
+        },
+      ];
+      toggleMenu = () => {
+        (this.state.menuVisible)?this.setState({menuVisible:false}):this.setState({menuVisible:true});
+      };
+    
+      onMenuItemSelect = (index) => {
+        // Handle Item Select
+    
+        this.setState({menuVisible : false,channel:this.menuData[index].title});
+      };
+    renderMenuAction = () => (
+        <OverflowMenu
+          visible={this.state.menuVisible}
+          data={this.menuData}
+          onSelect={this.onMenuItemSelect}
+          onBackdropPress={this.toggleMenu}
+          style = {{marginTop:30,}}
+        >
+          <TopNavigationAction
+            icon={MoreVerticalIconFill}
+            onPress={this.toggleMenu}
+          />
+        </OverflowMenu>
+      );
+      renderAddAction = () => (
+          <TopNavigationAction icon={PlusIconFill}/>
+      );
+    renderRightControls = ()=>{
+      MenuAction = this.renderMenuAction();
+      return [MenuAction];
+    }
     renderItemAccessory = (style) => (
         <Button appearance='ghost' status='danger' icon={HeartIconFill} onPress={this._signOutAsync}/>
     );
@@ -22,9 +70,12 @@ export default class ChannelScreen extends React.Component{
         return(
             <Layout>
                 <TopNavigation
-                    title='<channel Name>'
+                    title= {this.state.channel}
                     alignment='center'
+                    rightControls={this.renderRightControls()}
+                    leftControl = {this.renderAddAction()}
                     style ={{marginTop:25}}
+                    titleStyle = {{fontWeight:'bold',fontSize:20,lineHeight:40}}
                   />
                     <List
                         data={this.data}
