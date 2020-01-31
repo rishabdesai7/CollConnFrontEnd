3e5ab466de4cc59f76029588a9b3c88a358d8e9b
rishabdesai7 <rishabdesai7@gmail.com>
2020-01-31 10:22:48 +0530

    IpSet

--- CCN/src/urls/urlgenerator.js
+++ CCN/src/urls/urlgenerator.js
@@ -1,4 +1,7 @@
-import con from '../json/config';
-export var loginUrl = _=> {return 'http://'+con['ip']+':'+con['port']+'/colcon/login'}
-export var forgotPwdUrl = (id) => {return 'http://'+con['ip']+':'+con['port']+'/colcon/forgotpassword/'+id}
-export var resetPwdUrl = (id,pwd)=>{return 'http://'+con['ip']+':'+con['port']+'/colcon/resetpassword/'+id+'/'+pwd}
+import Constants from "expo-constants";
+const { manifest } = Constants;
+const uri = `http://${manifest.debuggerHost.split(':').shift()}:8000`;
+console.log(uri);
+export var loginUrl = _=> {return uri+'/colcon/login'}
+export var forgotPwdUrl = (id) => {return uri+'/colcon/forgotpassword/'+id}
+export var resetPwdUrl = (id,pwd)=>{return uri+'/colcon/resetpassword/'+id+'/'+pwd}
